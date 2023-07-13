ddf34a7feb334db1ec842264248d74513d2de6c6
kalshettigayatri <gayatrikalshetti3@gmail.com>
2023-07-13 18:06:40 +0530

    check leap year

--- Program.cs
+++ Program.cs
@@ -1,10 +1,16 @@
-﻿namespace FlipCoinDemo
+﻿namespace CheckLeapYear
 {
-    internal class Program
+    internal class LeapYear
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter Year : ");
+            int Year = int.Parse(Console.ReadLine());
+            if (((Year % 4 == 0) && (Year % 100 != 0)) || (Year % 400 == 0)) Console.WriteLine("{0} is a Leap Year.", Year);
+            else Console.WriteLine("{0} is not a Leap Year.", Year);
+            Console.ReadLine();
+
+
         }
     }
 }
