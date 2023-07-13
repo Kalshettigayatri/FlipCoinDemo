b9dcd1a27f09e52c93fec98037698f8fe593c7d8
kalshettigayatri <gayatrikalshetti3@gmail.com>
2023-07-13 17:59:14 +0530

    Welcome to FlipCoinDEmo

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FlipCoinDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
